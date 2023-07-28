d2868f58221f56b3d36309179620a8e82a3cc905
sohee <sohee17802@gamil.com>
2023-07-28 19:19:41 +0900

    lease 개인 커밋

---
+++ lease/src/main/java/july/lease/LeaseApplication.java
@@ -0,0 +1,15 @@
+package july.lease;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeaseApplication {
+
+	// 여기서만 실행
+	
+	public static void main(String[] args) {
+		SpringApplication.run(LeaseApplication.class, args);
+	}
+
+}
